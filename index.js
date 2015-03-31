73e6f078d036611900cc45010110edff88bcb1e9
voidy <yac_yu@163.com>
2015-03-31 22:48:55 +0800

    version 0.1

---
+++ index.js
@@ -0,0 +1,31 @@
+var http = require('http');           
+var socketio = require('socket.io');  
+var fs = require('fs');              
+
+var app = http.createServer(function(req, res) {
+ 
+    fs.readFile(__dirname + '/index.html', function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+        res.writeHead(200); 
+        res.write(data);   
+        res.end();
+    });
+}).listen(8888); 
+
+
+var io = socketio(app);
+
+
+io.on('connection', function (socket) {
+   
+    socket.on('chat', function (data) {
+        console.log(data);
+
+        io.emit('sendmsg', data); 
+    });
+});
+
+console.log("Server is running at http://localhost:8888")
